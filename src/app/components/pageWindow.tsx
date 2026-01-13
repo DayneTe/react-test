121ad776871dc45adfca58d8f97fc9b238e63694
DayneTes <dayne3672@gmail.com>
2026-01-13 16:56:11 -0500

    add draggable windows

--- src/app/components/pageWindow.tsx
+++ src/app/components/pageWindow.tsx
@@ -1,31 +1,83 @@
-import React, {  } from "react";
+import React, { useRef } from "react";
 
 interface windowProps {
   isOpen: boolean;
   onClose: () => void;
+  zCounter: (el: HTMLDivElement | null) => void
 }
 
-const PageWindow: React.FC<windowProps> = ({ isOpen, onClose }) => {
- 
+const PageWindow: React.FC<windowProps> = ({ isOpen, onClose, zCounter }) => {
+  const currWindow = useRef<HTMLDivElement>(null);
+  const isDragging = useRef(false);
+  const startX = useRef(0);
+  const startY = useRef(0);
+
+  const mouseMove = (e: MouseEvent) => {
+    if (!isDragging.current || !currWindow.current) return;
+
+    const newX = e.clientX - startX.current;
+    const newY = e.clientY - startY.current;
+
+    startX.current = e.clientX;
+    startY.current = e.clientY;
+
+    let nextLeft = currWindow.current.offsetLeft + newX;
+    let nextTop = currWindow.current.offsetTop + newY;
+
+    const maxLeft = window.innerWidth - currWindow.current.offsetWidth;
+    const maxTop = window.innerHeight - currWindow.current.offsetHeight;
+
+    nextLeft = Math.max(0, Math.min(nextLeft, maxLeft));
+    nextTop = Math.max(0, Math.min(nextTop, maxTop));
+
+    currWindow.current.style.left = nextLeft + "px";
+    currWindow.current.style.top = nextTop + "px";
+  };
+
+  const mouseUp = () => {
+    isDragging.current = false;
+    document.removeEventListener("mousemove", mouseMove);
+    document.removeEventListener("mouseup", mouseUp);
+  };
+
+  const mouseDown = (e: React.MouseEvent) => {
+    e.preventDefault();
+    if (!currWindow.current) return;
+
+    zCounter(currWindow.current);
+    isDragging.current = true;
+    startX.current = e.clientX;
+    startY.current = e.clientY;
+
+    document.addEventListener("mousemove", mouseMove);
+    document.addEventListener("mouseup", mouseUp);
+  };
 
   if (!isOpen) return null;
 
   return (
-    <div className="w-120 h-100 bg-gray-100 absolute top-1/4 left-1/4 border-2 border-blue-600 rounded-t-lg">
-      <div className="w-full h-7 bg-gradient-to-b from-blue-400 via-blue-600 via-20% to-blue-700 to-90% flex justify-end items-center gap-1">
+    <div
+      ref={currWindow}
+      onMouseDown={() => zCounter(currWindow.current)}
+      className="w-120 h-100 bg-gray-100 absolute top-1/4 left-1/4 border-2 border-blue-600 rounded-t-lg"
+    >
+      <div
+        onMouseDown={mouseDown}
+        className="w-full h-7 bg-gradient-to-b from-blue-400 via-blue-600 via-20% to-blue-700 to-90% flex justify-end items-center gap-1 select-none"
+      >
         <button className="w-[22px] h-[22px] bg-gradient-to-br from-blue-300 to-blue-600 to-35% border border-white text-white text-2xl leading-none font-bold rounded-sm flex items-center hover:from-blue-700 hover:to-blue-700">
-          <p className="pb-4 pl-1">_</p>
+          <span className="pb-4 pl-[5px]">_</span>
         </button>
 
         <button className="w-[22px] h-[22px] bg-gradient-to-br from-blue-300 to-blue-600 to-35% border border-white text-white text-2xl leading-none font-bold rounded-sm flex items-center hover:from-blue-700 hover:to-blue-700">
-          <p className="pb-2 pl-1">□</p>
+          <span className="pb-2 pl-[3px]">□</span>
         </button>
 
         <button
           className="w-[22px] h-[22px] bg-gradient-to-br from-red-300 to-red-500 to-35% border border-white text-white text-3xl leading-none rounded-sm flex items-center cursor-pointer hover:from-red-600 hover:to-red-600"
           onClick={onClose}
         >
-          <p className="pb-1">×</p>
+          <span className="pb-[6px]">×</span>
         </button>
       </div>
     </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,31 @@ import { useState } from "react";
 import Clock from "./components/clock";
 import Icon from "./components/icon";
 import PageWindow from "./components/pageWindow";
+let zIndexCount = 5;
 
 export default function Home() {
   const [windowOpen, setWindowOpen] = useState<boolean>(false);
+  const [window2Open, setWindow2Open] = useState<boolean>(false);
+
+  const bringToFront = (el: HTMLDivElement | null) => {
+    if (!el) return;
+    zIndexCount += 1;
+    el.style.zIndex = zIndexCount.toString();
+  };
 
   return (
     <main>
       <div className="bg-[url('/wallpaper.webp')] bg-cover bg-no-repeat w-screen h-screen flex items-end">
-        <PageWindow isOpen={windowOpen} onClose={() => setWindowOpen(false)}  />
+        <PageWindow
+          isOpen={windowOpen}
+          onClose={() => setWindowOpen(false)}
+          zCounter={bringToFront}
+        />
+        <PageWindow
+          isOpen={window2Open}
+          onClose={() => setWindow2Open(false)}
+          zCounter={bringToFront}
+        />
 
         <div className="grid grid-cols-1 gap-5 absolute self-start pt-4 pl-3">
           <Icon
@@ -18,7 +35,14 @@ export default function Home() {
             icon={"/vercel.svg"}
             onOpen={() => setWindowOpen(true)}
           />
+
+          <Icon
+            name={"coo.exe shortcut"}
+            icon={"/globe.svg"}
+            onOpen={() => setWindow2Open(true)}
+          />
         </div>
+
         <div className="bg-gradient-to-b from-blue-500 via-blue-600 via-20% to-blue-500 to-95% w-screen h-10 flex justify-between border-t-1 border-sky-600">
           <div className="bg-gradient-to-b from-green-400 via-green-600 to-green-500 flex items-center h-full w-33 rounded-r-2xl shadow-md border-r-2 border-green-800 pl-2 gap-2 cursor-pointer">
             <div className="w-8 h-8 bg-[url('/start.webp')] bg-cover bg-center" />
