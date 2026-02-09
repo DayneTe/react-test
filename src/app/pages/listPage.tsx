9605ad54bf1b1c8e4d8c9c6a5c31bf55c5e12190
DayneTes <dayne3672@gmail.com>
2026-02-09 00:11:50 -0500

    Style list and me page

--- src/app/pages/listPage.tsx
+++ src/app/pages/listPage.tsx
@@ -1,16 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import TheList from "../components/theList";
+import List2024 from "./theLists/list2024";
+import List2025 from "./theLists/list2025";
 
 const ListPage: React.FC = () => {
+
+    const [activeView, setActiveView] = useState<"home" | "2025" | "2024">("home");
+
     return (
         <div className="w-full h-full overflow-y-auto bg-neutral-100 text-black pt-6">
-            <h1 className="text-center text-4xl font-bold pt-6 pb-10">The List</h1>
-            <div className="grid grid-cols-[repeat(3,minmax(0,1fr))] justify-items-center gap-9">
-                <TheList listImg={"/globe.svg"} listName={"The List 2026"} />
-                <TheList listImg={"/globe.svg"} listName={"The List 2025"} />
-                <TheList listImg={"/vercel.svg"} listName={"The List 2025"} />
-                <TheList listImg={"/window.svg"} listName={"The List 2025"} />
-            </div>
+            {activeView === "home" && (
+                <>
+                    <h1 className="text-center text-4xl font-bold pt-6 pb-10">The List</h1>
+                    <div className="grid grid-cols-[repeat(3,minmax(0,1fr))] justify-items-center gap-9">
+                        <TheList listImg={"/globe.svg"} listName={"The List 2025"} openPage={() => setActiveView("2025")} />
+                        <TheList listImg={"/window.svg"} listName={"The List 2024"} openPage={() => setActiveView("2024")} />
+                    </div>
+                </>
+            )}
+
+            {activeView === "2025" && (
+                <>
+                    <List2025 onBack={() => setActiveView("home")} />
+                </>
+            )}
+
+            {activeView === "2024" && (
+                <>
+                    <List2024 onBack={() => setActiveView("home")} />
+                </>
+            )}
         </div>
     )
 }
--- src/app/pages/mePage.tsx
+++ src/app/pages/mePage.tsx
@@ -2,8 +2,28 @@ import React from "react";
 
 const MePage: React.FC = () => {
     return (
-        <div className="w-full h-full bg-amber-600 text-black pt-6">
-            <div className="bg-neutral-100 h-full w-[50%] mx-auto">uhh</div>
+        <div className="w-full h-full text-black pt-6 overflow-y-auto">
+            <div className="grid grid-cols-[minmax(0,210px)_1fr] grid-rows-[10%_1fr] h-full">
+                <div className="col-span-2 bg-amber-700 border-b-2 border-orange-900">
+                    Title
+                </div>
+                <div className="bg-amber-700 h-full border-r-2 border-orange-900">
+                    <div className="flex flex-col gap-15 pt-8">
+                        <button className="bg-orange-400 min-w-4/5 mx-auto border-2 rounded-md border-orange-900 py-3 px-5 hover:bg-orange-500 cursor-pointer text-black font-bold">
+                            Draw
+                        </button>
+                        <button className="bg-orange-400 min-w-4/5 mx-auto border-2 rounded-md border-orange-900 py-3 px-5 hover:bg-orange-500 cursor-pointer text-black font-bold">
+                            Cook
+                        </button>
+                        <button className="bg-orange-400 min-w-4/5 mx-auto border-2 rounded-md border-orange-900 py-3 px-5 hover:bg-orange-500 cursor-pointer text-black font-bold">
+                            Live
+                        </button>
+                    </div>
+                </div>
+                <div className="bg-orange-300 h-full">
+
+                </div>
+            </div>
         </div>
     )
 }
