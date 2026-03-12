a7a334f623474193a6d124ea5dd14e5a3695ee27
DayneTes <dayne3672@gmail.com>
2026-03-12 17:42:55 -0400

    add page arrows for xivpages

--- src/app/pages/ffPage.tsx
+++ src/app/pages/ffPage.tsx
@@ -1,29 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import Xiv312 from "./xivPages/xiv-3-12";
+import Xiv412 from "./xivPages/xiv-4-12";
 
 const FfPage: React.FC = () => {
+
+    const [activeView, setActiveView] = useState<"home" | "312" | "412">("home");
+
     return (
         <div className="bg-neutral-800 w-full h-full flex items-end">
-            <div className="bg-[#47332C] w-[97%] h-7/8 mx-auto mb-1 flex justify-between">
+            <div className="bg-[#47332C] w-full h-7/8 mx-auto mb-1 flex justify-center">
 
                 <div className="flex flex-col justify-evenly h-full pl-10">
-                    <div className="bg-blue-400 w-40 h-25 rotate-20">
+                    <div className="bg-blue-400 w-40 h-25 rotate-20 cursor-pointer" onClick={() => setActiveView("312")}>
                         <div className="bg-neutral-800 rounded-4xl w-4 h-4 mx-auto mt-1" />
+                        <p className="text-center">03/12/2026</p>
                     </div>
-                    <div className="bg-red-400 w-40 h-25 rotate-340">
+                    <div className="bg-red-400 w-40 h-25 rotate-340 cursor-pointer" onClick={() => setActiveView("412")}>
                         <div className="bg-neutral-800 rounded-4xl w-4 h-4 mx-auto mt-1" />
+                        <p className="text-center">04/12/2026</p>
                     </div>
-                    <div className="bg-yellow-400 w-40 h-25 rotate-8">
+                    <div className="bg-yellow-400 w-40 h-25 rotate-8 cursor-pointer" onClick={() => setActiveView("home")}>
                         <div className="bg-neutral-800 rounded-4xl w-4 h-4 mx-auto mt-1" />
+                        <p className="text-center">Home</p>
                     </div>
                 </div>
 
-                <div className="bg-neutral-800 w-[60%] max-w-[520px] h-4/5 mr-10 mt-19 content-center">
-                    <div className="bg-white w-10 h-5 absolute rotate-45 z-1 opacity-50 bottom-25 translate-x-3" />
-                    <div className="bg-white w-10 h-5 absolute rotate-45 z-1 opacity-50 right-18" />
-                    <div className="mask-[url(/Untitled2.png)] mask-cover bg-[#e4c789] w-[95%] max-w-[475px] h-[95%] mx-auto">
+                <div className="bg-neutral-800 w-[60%] max-w-[520px] h-4/5 mt-19 mx-auto">
+                    {activeView === "312" && <Xiv312 />}
+                    {activeView === "412" && <Xiv412 />}
 
-
-                    </div>
                 </div>
             </div>
         </div>
---
+++ src/app/pages/xivPages/xiv-4-12.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+
+const Xiv412: React.FC = () => {
+    const [page, setPage] = useState(0);
+
+    const content = [
+        "04/12/2026",
+        "dwa",
+        "way",
+    ]
+
+    const PER_PAGE = 1;
+    const totalPages = Math.ceil(content.length / PER_PAGE);
+    const visibleContent = content.slice(page * PER_PAGE, (page + 1) * PER_PAGE);
+
+    return (
+        <div className="bg-neutral-800 w-full h-full mt-5 relative">
+            <div className="bg-white w-10 h-5 absolute rotate-45 z-1 opacity-50 bottom-11 translate-x-3" />
+            <div className="bg-white w-10 h-5 absolute rotate-45 z-1 opacity-50 right-4" />
+            <div className="mask-[url(/page.png)] mask-cover bg-[#e4c789] w-[95%] max-w-[475px] h-[92%] mx-auto overflow-y-hidden">
+                <div className="w-full h-full pl-7 pt-4 space-y-2 leading-relaxed">
+                    {visibleContent.map((item, index) => (
+                        <div key={index}>{item}</div>
+                    ))}
+                </div>
+            </div>
+            <div className="flex justify-center gap-10">
+                <button onClick={() => setPage(page - 1)} disabled={page === 0} className="text-4xl cursor-pointer">←</button>
+                <button onClick={() => setPage(page + 1)} disabled={page === totalPages - 1} className="text-4xl cursor-pointer">→</button>
+            </div>
+        </div>
+    )
+}
+
+
+export default Xiv412;
