cfbcfa0b1695dab0221594263ed021363b1fee70
DayneTes <dayne3672@gmail.com>
2026-03-23 14:53:27 -0400

    Add cover to ffpage

--- src/app/pages/ffPage.tsx
+++ src/app/pages/ffPage.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Image from "next/image";
 import Xiv312 from "./xivPages/xiv-3-12";
 import Xiv412 from "./xivPages/xiv-4-12";
 
@@ -7,8 +8,8 @@ const FfPage: React.FC = () => {
     const [activeView, setActiveView] = useState<"home" | "312" | "412">("home");
 
     return (
-        <div className="bg-neutral-800 w-full h-full flex items-end">
-            <div className="bg-[#47332C] w-full h-7/8 mx-auto mb-1 flex justify-center">
+        <div className="bg-neutral-900 w-full h-full flex items-end overflow-clip">
+            <div className="bg-[#47332C] w-full h-7/8 mx-auto mb-5 flex justify-center">
 
                 <div className="flex flex-col justify-evenly h-full pl-10">
                     <div className="bg-blue-400 w-40 h-25 rotate-20 cursor-pointer" onClick={() => setActiveView("312")}>
@@ -25,10 +26,17 @@ const FfPage: React.FC = () => {
                     </div>
                 </div>
 
-                <div className="bg-neutral-800 w-[60%] max-w-[520px] h-4/5 mt-19 mx-auto">
+                <div className="bg-neutral-800 w-[60%] max-w-[520px] h-5/6 mt-19 mx-auto">
+                    {activeView === "home" && <>
+                        <div className="w-full h-full flex justify-center items-center">
+                            <div className="w-[95%] h-[95%] relative">
+                                <Image src={"/j.png"} alt={""} fill
+                                 />
+                            </div>
+                        </div>
+                    </>}
                     {activeView === "312" && <Xiv312 />}
                     {activeView === "412" && <Xiv412 />}
-
                 </div>
             </div>
         </div>
